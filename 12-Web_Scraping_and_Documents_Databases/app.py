7c35e22d79d30cd2913150e0527143b795c6f2ab
ryan taylor <lisaislas@yahoo.com>
2021-02-08 22:36:23 -0800

    web scraping hw

--- 12-Web_Scraping_and_Documents_Databases/app.py
+++ 12-Web_Scraping_and_Documents_Databases/app.py
@@ -1,37 +1,29 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars2
+from flask import Flask, render_template, redirect, jsonify
+import pymongo
+import scrape_mars
 
-# Create an instance of Flask
 app = Flask(__name__)
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
-
-# Route to render index.html template using data from Mongo
+# route
 @app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    mars = mongo.db.mars.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars = mars)
-
-
-# Route that will trigger the scrape function
+def index():
+    mars = client.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+    
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars2.scrape_info()
+    mars = client.db.mars
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars.update({}, mars_data, upsert=True)
+    mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- 12-Web_Scraping_and_Documents_Databases/scrape_mars.py
+++ 12-Web_Scraping_and_Documents_Databases/scrape_mars.py
@@ -1,199 +1,106 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
-import numpy as np
-import pandas as pd
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
-
+from pprint import pprint
+import pymongo
+import pandas as pd
 import requests
-import re
+from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
+from flask import Flask, render_template
+import time
+import numpy as np
+import json
+from selenium import webdriver
 
+def init_browser():
+    executable_path = {'executable_path' : ChromeDriverManager().install()}
+    return Browser('chrome', **executable_path, headless=False)
 
-
-def scrape_info():
-
+def scrape():
+    browser = init_browser()
     mars = {}
-
-
-
-
-
-    #get_ipython().system('which chromedriver')
-
-
-
-
-
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
-
-
-
-
-    url = 'https://mars.nasa.gov/news'
-    browser.visit(url)
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-
-
-
-
-    article = soup.find("div", class_="list_text")
-    news_title = article.find("div", class_="content_title").text
-    news_p = article.find("div", class_="article_teaser_body").text
-    print(news_title)
-    print(news_p)
-    mars["news_title"] = news_title
-    mars["news_p"] = news_p
-    #print(news_title)
-
-
-    # Featured Images
-
-
-
-
-    # url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    base_url = 'https://www.jpl.nasa.gov'
-    url = base_url + '/spaceimages/?search=&category=Mars'
-
-
-
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
 
     browser.visit(url)
+    time.sleep(1)
     html = browser.html
     soup = bs(html, 'html.parser')
+    #title
+    news_title = soup.title.text
+     #paragraph text
+    news_p = soup.find_all('p')
+    news_p_text = []
+    for paragraph in news_p:
+        news_p_text.append(paragraph.text)
 
+    # space image save
+    
+    # While chromedriver is open go to JPL's Featured Space Image page. 
+    url2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url2)
 
-
-
-    image_url = soup.find("a", class_= "button fancybox")["data-fancybox-href"]
-    featured_image_url = base_url + image_url
-    print(featured_image_url)
-    mars["featured_image"] = featured_image_url
-
-
-    # Mars Facts
-
-
-
-
-    url = 'https://space-facts.com/mars/'
-
-
-
-
-
-    tables = pd.read_html(url)
-    tables
-
-
-
-
-
-    mars_facts_df = tables[0]
-    mars_facts_df.columns = ['Fact', 'Value']
-    mars_facts_df['Fact'] = mars_facts_df['Fact'].str.replace(':', '')
-    mars_facts_df
-
-
-
-
-
-    mars_facts_df = tables[0]
-    mars_facts_df.columns = ['Fact', 'Value']
-    mars_facts_df['Fact'] = mars_facts_df['Fact'].str.replace(':', '')
-    mars_facts_df
-    mars_facts_html = mars_facts_df.to_html()
-    print(mars_facts_html)
-    mars["facts"] = mars_facts_html
-
-
-    # Mars Hemispheres
-
-    # In[14]:
-
-
-    base_url = 'https://astrogeology.usgs.gov'
-    url = base_url + '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-
-
-
-
-
-    browser.visit(url)
+    # Scrape the browser into soup and use soup to find the full resolution image of mars
+    # Save the image url to a variable called `featured_image_url`
     html = browser.html
     soup = bs(html, 'html.parser')
+    image = soup.find('img', class_="thumb")["src"]
+    img_url = "https://jpl.nasa.gov"+image
+    featured_image_url = img_url
+    
+    # Add the featured image url to the dictionary
+    mars["image"] = featured_image_url
 
-
-
-
-
-    items = soup.find_all('div', class_='item')
-
-
-
-
-
-    urls = []
-    titles = []
-    for item in items:
-        urls.append(base_url + item.find('a')['href'])
-        titles.append(item.find('h3').text.strip())
-    print(urls)
-    titles
-
-
-
-
-    browser.visit(urls[0])
-    html = browser.html
-    soup = bs(html, 'html.parser')
-    oneurl = base_url+soup.find('img', class_='wide-image')['src']
-    oneurl
-
-
-
-
-
-    image_urls = []
-    for oneurl in urls:
-        browser.visit(oneurl)
-        html = browser.html
-        soup = bs(html,'html.parser')
-        oneurl = base_url+soup.find('img',class_='wide-image')['src']
-        image_urls.append(oneurl)
-    image_urls
-
-
-
-
-
-    hemisphere_images_urls = []
-
-    for i in range(len(titles)):
-        hemisphere_images_urls.append({'title':titles[i],'image_url':image_urls[i]})
+   
+    
+   
+ 
+    url_new = 'https://space-facts.com/mars/'
+    response_new = requests.get(url_new)
+    soup_new = bs(response_new.text, 'html.parser')
+    mars_fact_table = soup_new.find_all('tr')
+    mars_fact_data = []
+    for tr in mars_fact_table:
+        mars_fact_data.append(tr.text)
         
-    hemisphere_images_urls
-    mars["hemispheres"] = hemisphere_images_urls
+    mars_fact_df = pd.DataFrame(mars_fact_data)
+    
+    mars_table = mars_fact_df[0].str.split(":", n=1, expand = True)
+    
+    mars_table = mars_table.rename(columns={ 0 : "Description", 1 : "Data"})
+    fin_table = mars_table.to_html(index=False)
+    fin_table = fin_table.replace("\n", "")
+    fin_table
+
+    
+    url4 = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url4)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    mars_hemis=[]
+
+    for i in range (4):
+        time.sleep(5)
+        images = browser.find_by_tag('h3')
+        images[i].click()
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        partial = soup.find("img", class_="wide-image")["src"]
+        img_title = soup.find("h2",class_="title").text
+        img_url = 'https://astrogeology.usgs.gov'+ partial
+        dictionary={"title":img_title,"img_url":img_url}
+        mars_hemis.append(dictionary)
+        browser.back()
+        
+
+    
+
+   
+    mars["title"] = news_title
+    mars["paragraph"] = news_p_text
+    
+    mars["table"] = fin_table
+    mars["hemisphere"] = mars_hemis
 
 
 
-
-    #for i in range(len(hemisphere_images_urls)):
-    # print(hemisphere_images_urls[i]['title'])
-        #print(hemisphere_images_urls[i]['image_url'] + '\n')
-
-
-
-
-
+    browser.quit()
     return mars
-
-if __name__ == "__main__":
-    print(scrape_info())
